9507bdf03e57a12729b96e068b550e05a7ddea9f
Saifullah Ahmed <saifullahahmed380@gmail.com>
2025-08-23 21:31:03 +0500

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { initSocket } from './utils/socket';
-import http from 'http';
-import path from 'path';
-import authRoutes from './routes/authRoutes';
-import roleRoutes from './routes/roleRoutes';
-import subRoleRoutes from './routes/subRolesRoutes'
-import employeeRoutes from './routes/employeeRoutes';
-import shiftRoutes from './routes/shift.routes';
-import departmentRoutes from './routes/department.routes';
-import subDepartmentRoutes from './routes/subDepartment.routes';
-import userRoutes from './routes/userRoutes'
-import attendanceRoutes from './routes/attendanceRoutes'
-import adminAttendanceRoutes from './routes/adminAttendanceRoutes';
-import hrRoutes from './routes/hrRoutes';
-import permissionRoutes from './routes/permissionRoutes'
-import impersonateRoutes from './routes/impersonateRoutes'
-import fixAttendanceRoutes from './routes/fixAttendanceRoutes'
-import notificationRoutes from './routes/notificationRoutes'
-import salaryRoutes from './routes/salaryRoutes'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { initSocket } from "./utils/socket";
+import http from "http";
+import path from "path";
+import authRoutes from "./routes/authRoutes";
+import roleRoutes from "./routes/roleRoutes";
+import subRoleRoutes from "./routes/subRolesRoutes";
+import employeeRoutes from "./routes/employeeRoutes";
+import shiftRoutes from "./routes/shift.routes";
+import departmentRoutes from "./routes/department.routes";
+import subDepartmentRoutes from "./routes/subDepartment.routes";
+import userRoutes from "./routes/userRoutes";
+import attendanceRoutes from "./routes/attendanceRoutes";
+import adminAttendanceRoutes from "./routes/adminAttendanceRoutes";
+import hrRoutes from "./routes/hrRoutes";
+import permissionRoutes from "./routes/permissionRoutes";
+import impersonateRoutes from "./routes/impersonateRoutes";
+import fixAttendanceRoutes from "./routes/fixAttendanceRoutes";
+import notificationRoutes from "./routes/notificationRoutes";
+import salaryRoutes from "./routes/salaryRoutes";
 
 // Load environment variables
 dotenv.config();
@@ -33,32 +33,36 @@ const server = http.createServer(app);
 initSocket(server);
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://pegahcm.netlify.app/"], // allowed origins
+    credentials: true, // if you need cookies/auth headers
+  })
+);
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/roles', roleRoutes);
-app.use('/api/sub-roles', subRoleRoutes)
-app.use('/api/employees', employeeRoutes);
-app.use('/api/shifts', shiftRoutes);
-app.use('/api/departments', departmentRoutes);
-app.use('/api/sub-departments', subDepartmentRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/attendance', attendanceRoutes)
-app.use('/api/fix-attendance', fixAttendanceRoutes)
-app.use('/api/admin', adminAttendanceRoutes)
-app.use('/api/hr', hrRoutes)
-app.use('/api/permissions', permissionRoutes)
-app.use('/api/impersonate', impersonateRoutes)
-app.use('/api/notifications', notificationRoutes)
-app.use('/api/salary', salaryRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/roles", roleRoutes);
+app.use("/api/sub-roles", subRoleRoutes);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/shifts", shiftRoutes);
+app.use("/api/departments", departmentRoutes);
+app.use("/api/sub-departments", subDepartmentRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/attendance", attendanceRoutes);
+app.use("/api/fix-attendance", fixAttendanceRoutes);
+app.use("/api/admin", adminAttendanceRoutes);
+app.use("/api/hr", hrRoutes);
+app.use("/api/permissions", permissionRoutes);
+app.use("/api/impersonate", impersonateRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/salary", salaryRoutes);
 
 // Basic route
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to PegaHCM API' });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to PegaHCM API" });
 });
 
 // Start server
