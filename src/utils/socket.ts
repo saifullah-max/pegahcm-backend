b89efbdd258782025099673731f33e928910a3cc
Saifullah Ahmed <saifullahahmed380@gmail.com>
2025-08-23 21:36:57 +0500

    updated

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,37 +1,36 @@
 // utils/socket.ts
-import { Server } from 'socket.io';
-import http from 'http';
+import { Server } from "socket.io";
+import http from "http";
 
 let io: Server;
 
 export function initSocket(server: http.Server) {
-    io = new Server(server, {
-        cors: {
-            origin: 'http://localhost:5173', // or your frontend domain in production
-            methods: ['GET', 'POST'],
-        },
+  io = new Server(server, {
+    cors: {
+      origin: ["http://localhost:5173", "https://pegahcm.netlify.app/"], // or your frontend domain in production
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("join", (userId) => {
+      console.log(`🧠 Socket ${socket.id} joined room: ${userId}`);
+      socket.join(userId);
     });
 
-    io.on('connection', (socket) => {
-        console.log('User connected:', socket.id);
-
-        socket.on('join', (userId) => {
-            console.log(`🧠 Socket ${socket.id} joined room: ${userId}`);
-            socket.join(userId);
-        });
-
-        socket.on('disconnect', () => {
-            console.log('User disconnected:', socket.id);
-        });
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
     });
+  });
 
-
-    return io;
+  return io;
 }
 
 export function getIO() {
-    if (!io) {
-        throw new Error('Socket.io not initialized!');
-    }
-    return io;
+  if (!io) {
+    throw new Error("Socket.io not initialized!");
+  }
+  return io;
 }
