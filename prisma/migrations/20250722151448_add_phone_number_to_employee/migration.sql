701d6d1435156ce232d983da67a650f96355614f
Saifullah Ahmed <saifullahahmed380@gmail.com>
2025-09-22 21:52:47 +0500

    removed migrations, added it in gitignore

--- prisma/migrations/20250722151448_add_phone_number_to_employee/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `employee` ADD COLUMN `phoneNumber` VARCHAR(191) NULL,
-    MODIFY `fatherName` VARCHAR(191) NULL;
--- prisma/migrations/20250805111640_add_subrole_level/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `level` on table `subrole` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `subrole` MODIFY `level` INTEGER NOT NULL;
